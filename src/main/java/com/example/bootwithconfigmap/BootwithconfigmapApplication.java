2fa5dbe7f18ecd753d23e157e42c37146bdf1e53
rahulbiswaser <rhl.biswas@gmail.com>
2021-09-23 12:00:29 +0530

    Discovery Implemented

--- src/main/java/com/example/bootwithconfigmap/BootwithconfigmapApplication.java
+++ src/main/java/com/example/bootwithconfigmap/BootwithconfigmapApplication.java
@@ -2,12 +2,21 @@ package com.example.bootwithconfigmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BootwithconfigmapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootwithconfigmapApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate resttemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/example/bootwithconfigmap/DemoController.java
+++ src/main/java/com/example/bootwithconfigmap/DemoController.java
@@ -2,8 +2,11 @@ package com.example.bootwithconfigmap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
@@ -13,6 +16,12 @@ public class DemoController {
 	
 	@Autowired
     private DemoConfig config;
+	
+	@Autowired
+	RestTemplate resttemplate;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
     @GetMapping("/greet")
     public String greet() {
@@ -31,6 +40,13 @@ public class DemoController {
     @GetMapping
     public String load() {
         return String.format(config.getMessage(), "", "");
+    }
+    
+    @GetMapping("/discover")
+    public String discover() {
+    	String url="http://springapptarget:8081/target";
+    	ResponseEntity<String> responseentity=resttemplate.getForEntity(url, String.class);    	
+        return responseentity.getBody();
     }
 
 }
